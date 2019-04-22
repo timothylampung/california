93dc65cfd26ea74c51857a17da0c10b9bd404a99
timothylampung <timothy.lampung@canang.com.my>
2019-04-22 09:53:30 +0800

    Minimized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -80,24 +80,6 @@ import { AppMegamenuComponent } from './app.megamenu.component';
 import { AppBreadcrumbComponent } from './app.breadcrumb.component';
 import { AppTopBarComponent } from './app.topbar.component';
 import { AppFooterComponent } from './app.footer.component';
-import { DashboardDemoComponent } from './demo/view/dashboarddemo.component';
-import { SampleDemoComponent } from './demo/view/sampledemo.component';
-import { FormsDemoComponent } from './demo/view/formsdemo.component';
-import { DataDemoComponent } from './demo/view/datademo.component';
-import { PanelsDemoComponent } from './demo/view/panelsdemo.component';
-import { OverlaysDemoComponent } from './demo/view/overlaysdemo.component';
-import { MenusDemoComponent } from './demo/view/menusdemo.component';
-import { MessagesDemoComponent } from './demo/view/messagesdemo.component';
-import { MiscDemoComponent } from './demo/view/miscdemo.component';
-import { EmptyDemoComponent } from './demo/view/emptydemo.component';
-import { ChartsDemoComponent } from './demo/view/chartsdemo.component';
-import { FileDemoComponent } from './demo/view/filedemo.component';
-import { DocumentationComponent } from './demo/view/documentation.component';
-
-import { CarService } from './demo/service/carservice';
-import { CountryService } from './demo/service/countryservice';
-import { EventService } from './demo/service/eventservice';
-import { NodeService } from './demo/service/nodeservice';
 import { BreadcrumbService } from './breadcrumb.service';
 
 @NgModule({
@@ -183,24 +165,11 @@ import { BreadcrumbService } from './breadcrumb.service';
     AppTopBarComponent,
     AppFooterComponent,
     AppProfileComponent,
-    DashboardDemoComponent,
-    SampleDemoComponent,
-    FormsDemoComponent,
-    DataDemoComponent,
-    PanelsDemoComponent,
-    OverlaysDemoComponent,
-    MenusDemoComponent,
-    MessagesDemoComponent,
-    MessagesDemoComponent,
-    MiscDemoComponent,
-    ChartsDemoComponent,
-    EmptyDemoComponent,
-    FileDemoComponent,
-    DocumentationComponent
+
   ],
   providers: [
     { provide: LocationStrategy, useClass: HashLocationStrategy },
-    CarService, CountryService, EventService, NodeService, BreadcrumbService
+    BreadcrumbService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,163 @@
+import {Component, AfterViewInit, ElementRef, Renderer2, ViewChild, OnDestroy} from '@angular/core';
+import {ScrollPanel} from 'primeng/primeng';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements AfterViewInit {
+
+  layoutMode = 'static';
+
+  megaMenuMode = 'dark';
+
+  menuMode = 'light';
+
+  profileMode = 'inline';
+
+  topbarMenuActive: boolean;
+
+  overlayMenuActive: boolean;
+
+  staticMenuDesktopInactive: boolean;
+
+  staticMenuMobileActive: boolean;
+
+  layoutMenuScroller: HTMLDivElement;
+
+  menuClick: boolean;
+
+  topbarItemClick: boolean;
+
+  activeTopbarItem: any;
+
+  resetMenu: boolean;
+
+  rightPanelActive: boolean;
+
+  rightPanelClick: boolean;
+
+  megaMenuActive: boolean;
+
+  megaMenuClick: boolean;
+
+  @ViewChild('layoutMenuScroller') layoutMenuScrollerViewChild: ScrollPanel;
+
+  constructor(public renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {this.layoutMenuScrollerViewChild.moveBar(); }, 100);
+  }
+
+  onLayoutClick() {
+    if (!this.topbarItemClick) {
+      this.activeTopbarItem = null;
+      this.topbarMenuActive = false;
+    }
+
+      if (!this.rightPanelClick) {
+          this.rightPanelActive = false;
+      }
+
+      if (!this.megaMenuClick) {
+          this.megaMenuActive = false;
+      }
+
+    if (!this.menuClick) {
+      if (this.overlayMenuActive || this.staticMenuMobileActive) {
+        this.hideOverlayMenu();
+      }
+    }
+
+    this.topbarItemClick = false;
+    this.menuClick = false;
+      this.rightPanelClick = false;
+      this.megaMenuClick = false;
+  }
+
+  onMenuButtonClick(event) {
+    this.menuClick = true;
+    this.topbarMenuActive = false;
+
+    if (this.layoutMode === 'overlay') {
+      this.overlayMenuActive = !this.overlayMenuActive;
+    } else {
+      if (this.isDesktop()) {
+        this.staticMenuDesktopInactive = !this.staticMenuDesktopInactive; } else {
+        this.staticMenuMobileActive = !this.staticMenuMobileActive; }
+    }
+
+    event.preventDefault();
+  }
+
+  onMenuClick($event) {
+    this.menuClick = true;
+    this.resetMenu = false;
+  }
+
+  onTopbarMenuButtonClick(event) {
+    this.topbarItemClick = true;
+    this.topbarMenuActive = !this.topbarMenuActive;
+
+    this.hideOverlayMenu();
+
+    event.preventDefault();
+  }
+
+  onTopbarItemClick(event, item) {
+    this.topbarItemClick = true;
+
+    if (this.activeTopbarItem === item) {
+      this.activeTopbarItem = null; } else {
+      this.activeTopbarItem = item; }
+
+    event.preventDefault();
+  }
+
+  onTopbarSubItemClick(event) {
+    event.preventDefault();
+  }
+
+    onRightPanelButtonClick(event) {
+        this.rightPanelClick = true;
+        this.rightPanelActive = !this.rightPanelActive;
+        event.preventDefault();
+    }
+
+    onRightPanelClick() {
+        this.rightPanelClick = true;
+    }
+
+    onMegaMenuButtonClick(event) {
+        this.megaMenuClick = true;
+        this.megaMenuActive = !this.megaMenuActive;
+        event.preventDefault();
+    }
+
+    onMegaMenuClick() {
+        this.megaMenuClick = true;
+    }
+
+  hideOverlayMenu() {
+    this.overlayMenuActive = false;
+    this.staticMenuMobileActive = false;
+  }
+
+  isTablet() {
+    const width = window.innerWidth;
+    return width <= 1024 && width > 640;
+  }
+
+  isDesktop() {
+    return window.innerWidth > 1024;
+  }
+
+  isMobile() {
+    return window.innerWidth <= 640;
+  }
+
+  isOverlay() {
+    return this.layoutMode === 'overlay';
+  }
+}
